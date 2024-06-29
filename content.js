bfe1f4df0422008fb2d6d2752df1c34d955a9c3d
Julius C. Cesar <knznsmn@icloud.com>
2024-06-29 19:54:12 +0400

    Add initial files

---
+++ content.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', function() {
+  let securityCode = document.getElementById('Button3').value;
+
+  document.getElementById('securityCodeTextboxID').value = securityCode;
+});
+
