3167385d2839c2b54ded622664fc183a3737603e
Julius C. Cesar <knznsmn@icloud.com>
2024-06-29 21:40:58 +0400

    Add autologin feature

--- content.js
+++ content.js
@@ -1,6 +1,12 @@
 window.addEventListener('load', function() {
+  document.getElementById('txtUsername').value = '';
+
+  document.getElementById('txtPassword').value = '';
+
   let securityCode = document.getElementById('Button3').value;
 
-  document.getElementById('securityCodeTextboxID').value = securityCode;
+  document.getElementById('txtotp').value = securityCode;
+
+  document.getElementById('btnEng').click();
 });
 
